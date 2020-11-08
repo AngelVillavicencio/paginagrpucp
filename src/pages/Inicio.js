0a2674e223aedf35a2a7c09dad7baf1c2471c1a4
Giancarlo <u20219910@utp.edu.pe>
2020-11-08 15:56:51 -0500

    Avance de vision

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -16,15 +16,19 @@ class Inicio extends Component {
 
 
             <section className="container-mision">
-
+            
 
 
 
             </section>
             <section className="container-vision">
-              <h2>Misión</h2>
-              <p>Generar la investigación y participación de proyectos en alumnos de la universidad y trabajo en conjunto con la universidad.</p>
-        
+            <h2 id="vision">
+              Visión
+              </h2>
+            <p id="vision-txt">
+              Ser la mejor opción dentro de la PUCP para adquirir y/o perfeccionar  abilidades técnicas mediante la participación en proyectos de robótica y difusión de conocimiento
+              </p>
+            <img src="https://assets.crucerosfluviales.es/ui/galery/barcos_expanded/planos_slider/noimg.jpg" id="img-vision" alt="imagen de la visión"></img>
 
 
             </section>
