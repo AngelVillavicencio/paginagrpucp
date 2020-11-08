e24edff52d48d9b034fc15f772a9c74bdd5f3b40
Angel Arturo villavicencio muñoz <villavicenciomunoz@gmail.com>
2020-11-08 16:48:59 -0500

    Agregue las paginas para contacto y Actividad-TAller

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import Inicio from "./pages/Inicio.js";
 import './App.css'
 import Actividades from "./pages/Actividades.js";
 import Miembros from "./pages/Miembros.js";
+import Actividad_taller  from "./pages/Actividad_taller.js";
 const App = () => {
-  return <Miembros></Miembros>;
+  return(
+  <React.Fragment>
+    <Actividad_taller></Actividad_taller>    
+  </React.Fragment>
+  );
 };
 
 export default App;
--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,7 +1,9 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import Footer from '../components/Footer';
-import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar';
+import imageMision from '../assets/images/Inicio/group.jpeg';
+import imageVision from '../assets/images/Inicio/group2.jpg';
 import './Inicio.css';
 class Inicio extends Component {
     render() {
@@ -17,22 +19,24 @@ class Inicio extends Component {
 
 
             <section className="container-mision">
-            <h2 id="mision">
-                  Misión 
+              <h2 id="mision">
+                Misión 
               </h2>
               <p id="mision-txt">
-                  Generar la investigación y participación de proyectos en alumnos de la universidad y trabajo en conjunto con la universidad.
+                Generar la investigación y participación de proyectos en alumnos de la universidad y trabajo en conjunto con la universidad.
               </p>
-              <img src="img-vision.jpeg" id="img-vision"></img>
-            
-
-
+              <img src={imageMision} id="img-vision"></img>
             </section>
             <section className="container-vision">
-
-        
-
-
+              <h2 id="mision">
+                Visión 
+              </h2>
+              <p id="mision-txt">
+                Ser la mejor opción dentro de la PUCP 
+                para adquirir y/o perfeccionar habilidades técnicas mediante 
+                la participación en proyectos de robótica y difusión de conocimiento
+              </p>
+              <img src={imageVision} id="img-vision"></img>
             </section>
             <section className="container-compromisos">
 
