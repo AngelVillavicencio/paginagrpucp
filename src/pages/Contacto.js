6e6dd80d59cdc678ff7e3c2c7d63ee5705a01465
Giancarlo <u20219910@utp.edu.pe>
2020-11-14 19:03:57 -0500

    Avance de Contacto/error de margin

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -7,7 +7,51 @@ class Contacto extends Component {
       return (
         <div>
             <Navbar></Navbar>
-            
+            {/* título */}
+            <div>
+              <h1>Contacto</h1>
+              <div></div>
+            </div>
+            {/* Recuadro */}
+            <div className="contenido">
+              {/* Form */}
+              <div className="formulario">
+                <h2>Envíanos un mensaje</h2>
+                <form>
+                <div>
+                  <label for="nombre">Nombre</label>
+                  <input type="text" name="nombre" id="nombre" placeholder="Ingrese su nombre"></input>
+                </div>
+                <div>
+                  <label for="correo">Correo</label>
+                  <input type="text" name="correo" id="correo" placeholder="Ingrese su correo"></input>
+                </div>
+                <div>
+                  <label for="mensaje">Mensaje</label>
+                  <textarea name="mensje" placeholder="Escriba su texto."></textarea>
+                </div>
+                <input type="submit" value="Enviar mensaje"></input>
+                </form>
+              </div>
+              {/* Información */}
+              <div className="info">
+                <h2>Información de contacto</h2>
+                <div>
+                  <h3>Dirección</h3>
+                  <p>Dirección fake</p>
+                </div>
+                <div>
+                  <h3>Teléfono</h3>
+                  <p>9549234449 - 934123424</p>
+                </div>
+                <div>
+                  <h3>Correo</h3>
+                  <p>MiPresidenteEsFelix,NoMerino@abajoelcongreso.edu.pe</p>
+                </div>
+              </div>
+            </div>
+
+
             <Footer></Footer>
         </div>
       );
